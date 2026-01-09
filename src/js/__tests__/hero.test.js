5858653ce513e22d021ddb4c6b4bfed3595d6062
ddmas <dimych1601@gamil.com>
2026-01-09 16:25:14 +0300

    Finish

---
+++ src/js/__tests__/hero.test.js
@@ -0,0 +1,44 @@
+import getSpecials from '../app'
+
+describe('getSpecials', () => {
+    test('возвращает спецатаки с описанием или значением по умолчанию', () => {
+        const character = {
+            "name": 'Лучник',
+            "type": 'Bowman',
+            "health": 50,
+            "level": 3,
+            "attack": 40,
+            "defence": 10,
+            "special": [
+                {
+                  id: 8,
+                  name: 'Двойной выстрел',
+                  icon: 'http://...',
+                  description: 'Двойной выстрел наносит двойной урон',
+                },
+                {
+                  id: 9,
+                  name: 'Нокаутирующий удар',
+                  icon: 'http://...',
+                },
+            ],
+        }
+
+        const result = getSpecials(character)
+
+        expect(result).toEqual([
+            {
+              id: 8,
+              name: 'Двойной выстрел',
+              icon: 'http://...',
+              description: 'Двойной выстрел наносит двойной урон',
+            },
+            {
+              id: 9,
+              name: 'Нокаутирующий удар',
+              icon: 'http://...',
+              description: 'Описание недоступно',
+            },
+        ])
+    })
+})
---
+++ src/js/app.js
@@ -0,0 +1,44 @@
+export default function getSpecials({ special }) {
+  return special.map(({ id, name, icon, description = 'Описание недоступно' }) => ({
+      id,
+      name,
+      icon,
+      description,
+  }))
+}
+
+const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+        {
+          id: 8,
+          name: 'Двойной выстрел',
+          icon: 'http://...',
+          description: 'Двойной выстрел наносит двойной урон'
+        },
+        {
+          id: 9,
+          name: 'Нокаутирующий удар',
+          icon: 'http://...'
+          // <- обратите внимание, описание "засекречено"
+        }
+    ]
+}
+
+const character2 = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: []
+}
+
+console.log(getSpecials(character))
+console.log(getSpecials(character2))
